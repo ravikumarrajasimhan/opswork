5fa0a3baaf37493de6371620b87be52b579f6221
ravikumarrajasimhan <ravikumar.rajasimhan@gmail.com>
2017-07-08 19:38:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
         <div align="center">
             <h1>Hello Cloud Gurus</h1>
             <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
-            <div align="center"><img src="https://s3.amazonaws.com/acloudguru-opsworkslab-donotdelete/logo-on-black-800.png"></div>
+            <div align="center"><img src=" https://s3.ap-south-1.amazonaws.com/agileindia-2017/Selfselection/20150912_170311.jpg"></div>
+           
             <p>Your ACloudGuru Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
             <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
         </div>
